b94521f76001d940c073ec663ef0b059bd03ed94
Erik Plaza Morales <erikplzm@gmail.com>
2026-03-12 10:16:13 +0100

    Creacion de la clase inicializadora de excepciones

---
+++ src/jcolonia/daw2025/tablasmvc/ExcepcionES.java
@@ -0,0 +1,11 @@
+package jcolonia.daw2025.tablasmvc;
+
+/**
+ * Clase encargada de validar datos de la tabla de multiplicar
+ */
+public class ExcepcionES extends Exception{
+	
+	public ExcepcionES(String mensaje) {
+		super(mensaje);
+	}
+}
